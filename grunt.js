0c8de264c3b53fb46d5e30f39eb75a16da4ceb31
Sergey Chikuyonok <serge.che@gmail.com>
2013-01-16 19:19:58 +0400

    Updated builder

--- grunt.js
+++ grunt.js
@@ -15,31 +15,33 @@ module.exports = function(grunt) {
 		},
 
 		frontend: {
-			css: {
-				src: './src/files/css',
-				dest: './out/c'
-			},
-			js: {
-				'./out/j/movie.js': [
-					'./src/files/codemirror-movie/src/codemirror2/codemirror.js',
-					'./src/files/codemirror-movie/src/codemirror2/css.js',
-					'./src/files/codemirror-movie/src/codemirror2/xml.js',
-					'./src/files/codemirror-movie/src/codemirror2/javascript.js',
-					'./src/files/codemirror-movie/src/codemirror2/htmlmixed.js',
-					'./src/files/codemirror-movie/src/lib/emmet.min.js',
-					'./src/files/codemirror-movie/dist/cm-movie-full.js'
-				],
+			main: {
+				css: {
+					src: './src/files/css',
+					dest: './out/c'
+				},
+				js: {
+					'./out/j/movie.js': [
+						'./src/files/codemirror-movie/src/codemirror2/codemirror.js',
+						'./src/files/codemirror-movie/src/codemirror2/css.js',
+						'./src/files/codemirror-movie/src/codemirror2/xml.js',
+						'./src/files/codemirror-movie/src/codemirror2/javascript.js',
+						'./src/files/codemirror-movie/src/codemirror2/htmlmixed.js',
+						'./src/files/codemirror-movie/src/lib/emmet.min.js',
+						'./src/files/codemirror-movie/dist/cm-movie-full.js'
+					],
 
-				'./out/j/main.js': [
-					'./src/files/js/movie-definition.js',
-					'./src/files/js/misc.js'
-				],
+					'./out/j/main.js': [
+						'./src/files/js/movie-definition.js',
+						'./src/files/js/misc.js'
+					],
 
-				'./out/j/cheatsheet.js': [
-					'./src/files/js/cheatsheet.js',
-					'./src/files/js/cheatsheet-css.js',
-					'./src/files/js/cheatsheet-controller.js'
-				]
+					'./out/j/cheatsheet.js': [
+						'./src/files/js/cheatsheet.js',
+						'./src/files/js/cheatsheet-css.js',
+						'./src/files/js/cheatsheet-controller.js'
+					]
+				}
 			}
 		}
 	});
